f9928d2bbc11536cec6c852b63698504e7e1ac6a
Ali Saleh <ali_saleh-4@hotmail.fr>
2023-05-15 15:21:24 +0200

    init api server

---
+++ server/database/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+exports.clientPromise = mongoose
+  .connect("mongodb://127.0.0.1:27017/Resalle", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connexion db ok !"))
+  .catch((err) => console.log(err));
